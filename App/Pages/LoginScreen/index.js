9c1fe6e5b6e3a36d3cf6312da11ecdd5a536b2eb
Muhammad Satrio Yudhistira <crisisaction097@gmail.com>
2022-10-07 01:24:59 +0700

    Membuat Tampilan Login Screen

--- App/Pages/LoginScreen/index.js
+++ App/Pages/LoginScreen/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import {View, Text, SafeAreaView, StyleSheet,TextInput , StatusBar, Image, TouchableOpacity} from 'react-native';
+import {View, Text, TextInput , Image, TouchableOpacity} from 'react-native';
 
 import {Loginlogo,Logokpcpen, Logokominfo, Logokemenkes, Logobumn} from '../../assets';
 
@@ -8,17 +8,21 @@ import {Loginlogo,Logokpcpen, Logokominfo, Logokemenkes, Logobumn} from '../../a
 export default LoginScreen =  ({navigation}) => {
 
     return (
-        <SafeAreaView style={{ flex:1 }}>
-         
-        <View style={{backgroundColor:'red',justifyContent:'center',alignItems:'center',marginTop:100}}>
+        
+         <View style={{ flex:1 }}>
+        <View style={{flex:1,
+        justifyContent:'flex-end'
+        ,alignItems:'center',
+        marginTop:10}}>
          <Image 
           style={{height:275,width:275}} 
           resizeMode={'stretch'}
           source={Loginlogo} />
            </View>
 
+
         <View style={{
-            backgroundColor:'blue',margin:20 
+            margin:10, 
         }}>
          <TextInput 
          style={{
@@ -27,10 +31,8 @@ export default LoginScreen =  ({navigation}) => {
             marginHorizontal:30,
             marginVertical:10,
             paddingHorizontal:20,
-            borderRadius:10,
-         }} 
-                    placeholder="Youremail@domain.com"
-                />
+            borderRadius:10,}}
+            placeholder="Youremail@domain.com"/>
 
         <TextInput style={{
             borderWidth:1,
@@ -38,19 +40,75 @@ export default LoginScreen =  ({navigation}) => {
             marginHorizontal:30,
             marginVertical:10,
             paddingHorizontal:20,
-            borderRadius:10,
-        }} 
-        placeholder="Password"/>
+            borderRadius:10,}} 
+            placeholder="Password"/>
 
         </View>
-        
-        <View style={{backgroundColor:'purple'}}>
-          
+
+        <TouchableOpacity onPress={()=>navigation.navigate('SplashScreen')}>
+        <View style={{
+            backgroundColor:'darkslategrey',justifyContent:'center',alignItems:'center',
+            padding:15,
+            marginHorizontal:60,
+            marginTop:10,
+            borderWidth:0,
+            borderRadius:10
+            }}>
+         
+                        <Text style={{
+                            textAlign:'center',
+                            color:'white',
+                            fontSize:20,
+                        }}>Login</Text>      
+        </View>
+        </TouchableOpacity>
+
+
+        <View>
+        <Text style={{
+          marginStart:20,
+          fontSize:15,
+          marginBottom:3,
+          marginTop:175
+          }}>Bekerjasama Dengan : </Text>
         </View>
 
+        <View style={{
+          flexDirection:'row',
+          justifyContent:'center',
+          alignItems:'center',}}>
+
+        <View style={{padding:10}}>
+         <Image 
+          style={{width:100,height:100}} 
+          resizeMode={'contain'}
+          source={Logokpcpen} />
+           </View>
+
+            <View style={{padding:10}}>
+         <Image 
+          style={{width:100,height:50}} 
+          resizeMode={'contain'}
+          source={Logokominfo} />
+           </View>
+           
+              <View style={{padding:10}}>
+         <Image 
+          style={{width:100,height:50}} 
+          resizeMode={'contain'}
+          source={Logokemenkes} />
+           </View>
+
+              <View style={{padding:10}}>
+         <Image 
+          style={{width:100,height:40}} 
+          resizeMode={'contain'}
+          source={Logobumn} />
+           </View>
+
+           </View>
 
 
-
-        </SafeAreaView>
+        </View>
     );
 }
