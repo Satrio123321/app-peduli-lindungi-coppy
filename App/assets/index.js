3ef0a6ab4a77c54d5edbdc96c13aa19d419406f3
Muhammad Satrio Yudhistira <crisisaction097@gmail.com>
2022-10-07 12:39:26 +0700

    penambahan import logo untuk menuscreen

--- App/assets/index.js
+++ App/assets/index.js
@@ -6,9 +6,22 @@ import Logokemenkes from './img/logo_kemenkes.png';
 import Logobumn from './img/logo_bumn.png';
 import Loginlogo from './img/logo-top.png';
 
+import LogoEntering from './img/ic_hand_qr.jpg';
+
+import LogoVaccine from './img/ic_vaccine_and_im_muniazation.jpg';
+import LogoCovid from './img/ic_covid_test_result.jpg';
+import LogoEhac from './img/ic_ehac.jpg';
+import LogoTravel from './img/ic_travel_regulation.jpg';
+import LogoTelemedicine from './img/ic_telemedicine.jpg';
+import LogoHealthcare from './img/ic_healthcare_facility.jpg';
+import LogoCovid19 from './img/ic_statistic_covid.jpg';
+import LogoFind from './img/ic_find_hospital.jpg';
+
 export{
 IconSplash,
 Logokpcpen,Logokominfo,Logokemenkes,Logobumn,
 Loginlogo,
+LogoEntering,
+LogoVaccine,LogoCovid,LogoEhac,LogoTravel,LogoTelemedicine,LogoHealthcare,LogoCovid19,LogoFind
 
 };
