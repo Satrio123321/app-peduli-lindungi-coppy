d6cdfb17741d79fae6438a92239b28fcf305a461
Muhammad Satrio Yudhistira <crisisaction097@gmail.com>
2022-10-08 19:18:18 +0700

    perapihan coding

--- App/Pages/SplashScreen/index.js
+++ App/Pages/SplashScreen/index.js
@@ -1,84 +1,84 @@
+import * as React from "react";
+import { View, Text, Image, StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { IconSplash } from "../../assets";
+import { Logokpcpen, Logokominfo, Logokemenkes, Logobumn } from "../../assets";
 
-import * as React from 'react';
-import {View, Text,  Image,StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import {IconSplash} from '../../assets';
-import {Logokpcpen, Logokominfo, Logokemenkes, Logobumn} from '../../assets';
-
-export default SplashScreen = ({navigation}) => {
+export default SplashScreen = ({ navigation }) => {
   React.useEffect(() => {
     setTimeout(() => {
-      navigation.replace('LoginScreen')
-     }, 3000)
-    }, [navigation]);
+      navigation.replace("LoginScreen");
+    }, 3000);
+  }, [navigation]);
 
-return (
-  <SafeAreaView style={{ flex: 1 }}>
-    
-    <StatusBar
-        barStyle="light-content"
-        animated={true}
-      color="#61dafb" />
-    
-        <View style={{
-          flex:1,
-          alignItems:'center',
-          justifyContent: 'center'}}>
-      
-        <Image source={IconSplash} 
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content" animated={true} color="#61dafb" />
+
+      <View
         style={{
-          resizeMode:'center',}}/>
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        <Image
+          source={IconSplash}
+          style={{
+            resizeMode: "center",
+          }}
+        />
+      </View>
 
+      <View>
+        <Text
+          style={{
+            marginStart: 20,
+            fontSize: 15,
+            marginBottom: 3,
+          }}>
+          Bekerjasama Dengan :{" "}
+        </Text>
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 25,
+        }}>
+        <View style={{ padding: 10 }}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            resizeMode={"contain"}
+            source={Logokpcpen}
+          />
         </View>
 
-        <View>
-        <Text style={{
-          marginStart:20,
-          fontSize:15,
-          marginBottom:3,
-          }}>Bekerjasama Dengan : </Text>
+        <View style={{ padding: 10 }}>
+          <Image
+            style={{ width: 100, height: 50 }}
+            resizeMode={"contain"}
+            source={Logokominfo}
+          />
         </View>
 
+        <View style={{ padding: 10 }}>
+          <Image
+            style={{ width: 100, height: 50 }}
+            resizeMode={"contain"}
+            source={Logokemenkes}
+          />
+        </View>
 
-        <View style={{
-          flexDirection:'row',
-          justifyContent:'center',
-          alignItems:'center',
-          marginBottom:25,}}>
-
-        <View style={{padding:10}}>
-         <Image 
-          style={{width:100,height:100}} 
-          resizeMode={'contain'}
-          source={Logokpcpen} />
-           </View>
-
-            <View style={{padding:10}}>
-         <Image 
-          style={{width:100,height:50}} 
-          resizeMode={'contain'}
-          source={Logokominfo} />
-           </View>
-           
-              <View style={{padding:10}}>
-         <Image 
-          style={{width:100,height:50}} 
-          resizeMode={'contain'}
-          source={Logokemenkes} />
-           </View>
-
-              <View style={{padding:10}}>
-         <Image 
-          style={{width:100,height:40}} 
-          resizeMode={'contain'}
-          source={Logobumn} />
-           </View>
-
-           </View>
-
-        
-</SafeAreaView>
-         
-          
-    );
-    };
+        <View style={{ padding: 10 }}>
+          <Image
+            style={{ width: 100, height: 40 }}
+            resizeMode={"contain"}
+            source={Logobumn}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
