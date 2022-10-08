e05dd8c40bb56a385d19fe6399503d525cd805ac
Muhammad Satrio Yudhistira <crisisaction097@gmail.com>
2022-10-08 19:16:57 +0700

    penambahan routes untuk vaccine screen

--- App/Routes.js
+++ App/Routes.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {
-  SplashScreen, 
-  LoginScreen,
-} from './Pages';
-import HomeScreen from './Pages/HomeScreen';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { SplashScreen, LoginScreen, HomeScreen, VaccineScreen } from "./Pages";
 
 const Stack = createStackNavigator();
 
 export default Router = () => {
   return (
     <Stack.Navigator initialRouteName="SplashScreen">
-     <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}} />
-     <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}} />
-     <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="VaccineScreen"
+        component={VaccineScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
-       );
-}; 
+  );
+};
