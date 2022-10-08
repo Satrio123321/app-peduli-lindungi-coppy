c5fb4bcdddc8b312f37198e42485bec5d3bdbed2
Muhammad Satrio Yudhistira <crisisaction097@gmail.com>
2022-10-08 19:17:28 +0700

    penambahan vaccine screen

--- App/Pages/index.js
+++ App/Pages/index.js
@@ -1,9 +1,6 @@
-import SplashScreen from './SplashScreen';
-import LoginScreen from './LoginScreen';
-import HomeScreen from './HomeScreen';
+import SplashScreen from "./SplashScreen";
+import LoginScreen from "./LoginScreen";
+import HomeScreen from "./HomeScreen";
+import VaccineScreen from "./VaccineScreen";
 
-export {
-    SplashScreen,
-    LoginScreen,
-    HomeScreen,
-};
+export { SplashScreen, LoginScreen, HomeScreen, VaccineScreen };
