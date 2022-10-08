e7e28c04ab9126d8ad1be3606ff6166bed2ec6da
Muhammad Satrio Yudhistira <crisisaction097@gmail.com>
2022-10-08 19:16:27 +0700

    penambahan logo

--- App/assets/index.js
+++ App/assets/index.js
@@ -1,27 +1,43 @@
-import IconSplash from './img/logo-with-text1.png';
+import IconSplash from "./img/logo-with-text1.png";
 
-import Logokpcpen from './img/logo_kpcpen.png';
-import Logokominfo from './img/logo_kominfo.png';
-import Logokemenkes from './img/logo_kemenkes.png';
-import Logobumn from './img/logo_bumn.png';
-import Loginlogo from './img/logo-top.png';
+import Logokpcpen from "./img/logo_kpcpen.png";
+import Logokominfo from "./img/logo_kominfo.png";
+import Logokemenkes from "./img/logo_kemenkes.png";
+import Logobumn from "./img/logo_bumn.png";
+import Loginlogo from "./img/logo-top.png";
 
-import LogoEntering from './img/ic_hand_qr.jpg';
+import LogoEntering from "./img/ic_hand_qr.jpg";
 
-import LogoVaccine from './img/ic_vaccine_and_immuniazation.jpg';
-import LogoCovid from './img/ic_covid_test_result.jpg';
-import LogoEhac from './img/ic_ehac.jpg';
-import LogoTravel from './img/ic_travel_regulation.jpg';
-import LogoTelemedicine from './img/ic_telemedicine.jpg';
-import LogoHealthcare from './img/ic_healthcare_facility.jpg';
-import LogoCovid19 from './img/ic_statistic_covid.jpg';
-import LogoFind from './img/ic_find_hospital.jpg';
+import LogoVaccine from "./img/ic_vaccine_and_immuniazation.jpg";
+import LogoCovid from "./img/ic_covid_test_result.jpg";
+import LogoEhac from "./img/ic_ehac.jpg";
+import LogoTravel from "./img/ic_travel_regulation.jpg";
+import LogoTelemedicine from "./img/ic_telemedicine.jpg";
+import LogoHealthcare from "./img/ic_healthcare_facility.jpg";
+import LogoCovid19 from "./img/ic_statistic_covid.jpg";
+import LogoFind from "./img/ic_find_hospital.jpg";
 
-export{
-IconSplash,
-Logokpcpen,Logokominfo,Logokemenkes,Logobumn,
-Loginlogo,
-LogoEntering,
-LogoVaccine,LogoCovid,LogoEhac,LogoTravel,LogoTelemedicine,LogoHealthcare,LogoCovid19,LogoFind
+import LogoVacCovid from "./img/ic_covid.jpg";
+import LogoVacCheckin from "./img/ic_check_in.jpg";
+import LogoVacChild from "./img/ic_child.jpg";
 
+export {
+  IconSplash,
+  Logokpcpen,
+  Logokominfo,
+  Logokemenkes,
+  Logobumn,
+  Loginlogo,
+  LogoEntering,
+  LogoVaccine,
+  LogoCovid,
+  LogoEhac,
+  LogoTravel,
+  LogoTelemedicine,
+  LogoHealthcare,
+  LogoCovid19,
+  LogoFind,
+  LogoVacCovid,
+  LogoVacCheckin,
+  LogoVacChild,
 };
